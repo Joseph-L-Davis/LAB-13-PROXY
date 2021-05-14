5f935800c0bdc82711b6a9478080d9350f5564c9
Joseph Davis <joethebartenderpdx@gmail.com>
2021-05-14 15:23:21 -0700

    removed all yelp trash

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,7 +1,6 @@
 import { locationData } from '../data/location-data.js';
 import { weatherData } from '../data/weather-data.js';
-import { yelpData } from '../data/yelp-data.js';
-import { formatLocation, formatWeather, formatYelp } from '../lib/utils.js';
+import { formatLocation, formatWeather } from '../lib/utils.js';
 
 
 describe('location Routes', () => {
@@ -25,7 +24,7 @@ describe('location Routes', () => {
     expect(output).toEqual(expectedLocations);
   });
 
-});
+}); 
 
 describe('weather Routes', () => {
 
@@ -44,7 +43,7 @@ describe('weather Routes', () => {
   // If a GET request is made to /api/cats, does:
   // 1) the server respond with status of 200
   // 2) the body match the expected API data?
-  it('Munge weather', async () => {
+  it('Munge weather', async () => { 
     // act - make the request
     const output = formatWeather(weatherData);
 
@@ -53,34 +52,3 @@ describe('weather Routes', () => {
 
 });
 
-describe('Yelp Routes', () => {
-
-  const expectedYelp = 
-    [
-      {
-        'name': 'Timberwood Grill',
-        'image': 'https://s3-media4.fl.yelpcdn.com/bphoto/tYLDE6HRJuNmb5FGOrjSuA/o.jpg',
-        'price': '$$',
-        'rating': 4,
-        'url': 'https://www.yelp.com/biz/timberwood-grill-charlottesville?adjust_creative=k9RolU7Bt9LyKvMZ0hzjLw&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=k9RolU7Bt9LyKvMZ0hzjLw'
-      },
-      {
-        'name': 'Martin\'s Grill',
-        'image': 'https://s3-media2.fl.yelpcdn.com/bphoto/jqkM52dRUPzDmVMU8rGjzg/o.jpg',
-        'price': '$',
-        'rating': 4,
-        'url': 'https://www.yelp.com/biz/martins-grill-charlottesville?adjust_creative=k9RolU7Bt9LyKvMZ0hzjLw&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=k9RolU7Bt9LyKvMZ0hzjLw'
-      }
-    ];
-
-  // If a GET request is made to /api/cats, does:
-  // 1) the server respond with status of 200
-  // 2) the body match the expected API data?
-  it('Munge Yelp', async () => {
-    // act - make the request
-    const output = formatYelp(yelpData);
-
-    expect(output).toEqual(expectedYelp);
-  });
-
-});
